595c194322c6568a4016b1e1ec9605cde062e067
Xellarant <xellarant@gmail.com>
2018-08-14 04:37:39 -0700

    kinda threw in all of the code in one big lump after the fact (since I realized I'd probably need to share the source code)

---
+++ src/NubChain.java
@@ -0,0 +1,70 @@
+import com.google.gson.GsonBuilder;
+import org.apache.commons.lang3.time.StopWatch;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+public class NubChain {
+
+    // stopwatch to keep track of how long the mining takes at each difficulty level
+    static StopWatch stopwatch = new StopWatch();
+
+    public static ArrayList<Block> blockchain = new ArrayList<Block>();
+    public static int difficulty = 5;
+
+    public static void main(String[] args) {
+        stopwatch.start();
+
+        //add our blocks to the blockchain ArrayList:
+
+        blockchain.add(new Block("Hi im the first block", "0"));
+        System.out.println("Trying to Mine block 1... ");
+        blockchain.get(0).mineBlock(difficulty);
+
+        blockchain.add(new Block("Yo im the second block",blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 2... ");
+        blockchain.get(1).mineBlock(difficulty);
+
+        blockchain.add(new Block("Hey im the third block",blockchain.get(blockchain.size()-1).hash));
+        System.out.println("Trying to Mine block 3... ");
+        blockchain.get(2).mineBlock(difficulty);
+
+        System.out.println("\nBlockchain is Valid: " + isChainValid());
+
+        String blockchainJson = new GsonBuilder().setPrettyPrinting().create().toJson(blockchain);
+        System.out.println("\nThe block chain: ");
+        System.out.println(blockchainJson);
+        stopwatch.stop();
+        long timeTaken = stopwatch.getTime(TimeUnit.SECONDS);
+        System.out.println("\nTime Elapsed: " + timeTaken + " seconds.");
+    }
+
+    public static Boolean isChainValid() {
+        Block currentBlock;
+        Block previousBlock;
+        String hashTarget = new String(new char[difficulty]).replace('\0', '0');
+
+        //loop through blockchain to check hashes:
+        for(int i=1; i < blockchain.size(); i++) {
+            currentBlock = blockchain.get(i);
+            previousBlock = blockchain.get(i-1);
+            //compare registered hash and calculated hash:
+            if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
+                System.out.println("Current Hashes not equal");
+                return false;
+            }
+            //compare previous hash and registered previous hash
+            if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
+                System.out.println("Previous Hashes not equal");
+                return false;
+            }
+            //check if hash is solved
+            if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
+                System.out.println("This block hasn't been mined");
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
